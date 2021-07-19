b3092068108e419716f4a96a91c1370a71da0be0
Lucas Cabral <lucas_cabral.deve@outlook.com>
2021-07-19 09:50:01 -0300

    CustomView class done.

--- app/src/main/java/com/lucascabral/dateextensions/ProgressButton.kt
+++ app/src/main/java/com/lucascabral/dateextensions/ProgressButton.kt
@@ -21,13 +21,59 @@ class ProgressButton @JvmOverloads constructor(
         object Loading : ProgressButtonState(false, View.VISIBLE)
     }
 
-    private var state = ProgressButtonState.Normal
+    private var state: ProgressButtonState = ProgressButtonState.Normal
         set(value) {
             field = value
             refreshState()
         }
 
-    private fun refreshState() {
+    init {
+        setLayout(attrs)
+        refreshState()
+    }
 
+    fun setLoading() {
+        state = ProgressButtonState.Loading
+    }
+
+    fun setNormal() {
+        state = ProgressButtonState.Normal
+    }
+
+    private fun setLayout(attrs: AttributeSet?) {
+        attrs?.let { attributeSet ->
+            val attributes = context.obtainStyledAttributes(
+                attributeSet,
+                R.styleable.ProgressButton
+            )
+
+            setBackgroundResource(R.drawable.progress_button_background)
+
+            val titleResId = attributes.getResourceId(R.styleable.ProgressButton_progress_button_title, 0)
+            if (titleResId != 0) title = context.getString(titleResId)
+
+            val loadingTitleResId = attributes.getResourceId(R.styleable.ProgressButton_progress_button_loading_title, 0)
+            if (loadingTitleResId != 0) loadingTitle = context.getString(loadingTitleResId)
+
+            attributes.recycle()
+        }
+    }
+
+    private fun refreshState() {
+        isEnabled = state.isEnabled
+        isClickable = state.isEnabled
+        refreshDrawableState()
+
+        binding.buttonTitleTextView.run {
+            isEnabled = state.isEnabled
+            isClickable = state.isEnabled
+        }
+
+        binding.buttonProgressBar.visibility = state.progressVisibility
+
+        when(state) {
+            ProgressButtonState.Normal -> binding.buttonTitleTextView.text = title
+            ProgressButtonState.Loading -> binding.buttonTitleTextView.text = loadingTitle
+        }
     }
 }
