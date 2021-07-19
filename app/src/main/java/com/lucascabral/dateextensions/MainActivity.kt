3109e313afbc201a0c6a358e36c008adbbe09438
Lucas Cabral <lucas_cabral.deve@outlook.com>
2021-07-19 10:38:04 -0300

    Layout xml done.

--- app/src/main/java/com/lucascabral/dateextensions/MainActivity.kt
+++ app/src/main/java/com/lucascabral/dateextensions/MainActivity.kt
@@ -2,10 +2,27 @@ package com.lucascabral.dateextensions
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import com.lucascabral.dateextensions.databinding.ActivityMainBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.currentTimeStampButton.setOnClickListener {
+            binding.currentTimeStampButton.setLoading()
+            lifecycleScope.launch {
+                delay(3000)
+                binding.currentTimeStampTextView.text = "time Stamp"
+                binding.currentTimeStampButton.setNormal()
+            }
+        }
     }
 }
