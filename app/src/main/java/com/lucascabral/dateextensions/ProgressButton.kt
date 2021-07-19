57db4e1b13e9cca53cf13e752922dc450e5a8eae
Lucas Cabral <lucas_cabral.deve@outlook.com>
2021-07-19 09:24:41 -0300

    customView control state value (normal or Loading) created.

--- app/src/main/java/com/lucascabral/dateextensions/ProgressButton.kt
+++ app/src/main/java/com/lucascabral/dateextensions/ProgressButton.kt
@@ -3,6 +3,7 @@ package com.lucascabral.dateextensions
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.lucascabral.dateextensions.databinding.ProgressButtonBinding
 
@@ -14,4 +15,19 @@ class ProgressButton @JvmOverloads constructor(
     private var loadingTitle: String? = null
 
     private val binding = ProgressButtonBinding.inflate(LayoutInflater.from(context), this, true)
+
+    sealed class ProgressButtonState(var isEnabled: Boolean, val progressVisibility: Int) {
+        object Normal : ProgressButtonState(true, View.GONE)
+        object Loading : ProgressButtonState(false, View.VISIBLE)
+    }
+
+    private var state = ProgressButtonState.Normal
+        set(value) {
+            field = value
+            refreshState()
+        }
+
+    private fun refreshState() {
+
+    }
 }
